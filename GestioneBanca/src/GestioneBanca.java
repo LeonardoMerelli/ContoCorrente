95815782691466448227aa1a8c283b319d0594a7
LeonardoMerelli <93209606+LeonardoMerelli@users.noreply.github.com>
2021-10-26 17:37:38 +0200

    Update GestioneBanca.java

--- GestioneBanca/src/GestioneBanca.java
+++ GestioneBanca/src/GestioneBanca.java
@@ -1,15 +1,1 @@
-import java.util.Scanner;
-import java.lang.String;
-public class GestioneBanca {
-	public static void main(String[] args) {
-		System.out.println("Inserire nome, cognome e password");
-		Scanner in = new Scanner(System.in);
-		String[] nome = new String[100];
-		String[] cognome = new String[100];
-		String[] password = new String[100];
-		nome = in.next();
-		cognome = in.next();
-		password = in.next();
-		TerminaleSportello.TerminaleSportello(nome, cognome, password);
-	}
-}
+
